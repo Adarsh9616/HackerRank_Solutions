603117cee51d34f44eefc780fe7089b987ad2b98
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-03 02:50:17 +0530

    Add files via upload

---
+++ Array Manipulation.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] q,int m) 
+    {
+        int l;
+        int u;
+        int k;
+        long sum=0;
+        long ar[]=new long[n];
+        for(int i=0;i<m;i++)
+        {
+            l=q[i][0];
+            u=q[i][1];
+            k=q[i][2];
+            ar[l-1]=ar[l-1]+k;
+            if(u<n)
+            {
+                ar[u]=ar[u]-k;
+            }
+        }
+        long max=Long.MIN_VALUE;
+
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+ar[i];
+            if(sum>max)
+            {
+                max=sum;
+            }
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
