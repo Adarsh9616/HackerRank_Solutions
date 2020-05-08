c0dd0e611c365482769ae9d3fbd67c2e78f7d188
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-09 01:12:53 +0530

    Add files via upload

---
+++ Java Factory Pattern.java
@@ -0,0 +1,11 @@
+
+            switch(order)
+            {
+                case "pizza": return new Pizza();
+                case "cake" : return new Cake();
+                default : return null;
+            }
+          
+        
+    
+
