153a63a90d628086d82858685987b026270cd1c0
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-08 23:41:40 +0530

    Add files via upload

---
+++ Valid Username Regular Expression.java
@@ -0,0 +1,8 @@
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[[A-Z]|[a-z]][[A-Z]|[a-z]|\\d|[_]]{7,29}$";
+}
+
