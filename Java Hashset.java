90a659e43f2c398e4e1ca13b331a4552e96dbbf1
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-13 03:38:51 +0530

    Add files via upload

---
+++ Java Hashset.java
@@ -0,0 +1,9 @@
+
+        Set<String> st=new HashSet<String>();
+        for(int i=0;i<t;i++)
+        {
+            st.add(pair_left[i]+" "+pair_right[i]);
+            System.out.println(st.size());
+        }
+//Write your code here
+
