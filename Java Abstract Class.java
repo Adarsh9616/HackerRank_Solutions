1a40e14820aff6ee4821a0644c66feeb29eaee32
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-18 01:03:06 +0530

    Add files via upload

---
+++ Java Abstract Class.java
@@ -0,0 +1,16 @@
+
+class MyBook extends Book
+{
+    String title;
+    @Override
+    void setTitle(String s)
+    {
+        title=s;
+    }
+    @Override
+    String getTitle(){
+        return title;
+    }
+}
+//Write MyBook class here
+
