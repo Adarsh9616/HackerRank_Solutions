f65ebdfd86efaa3bdfab0976378344eba1646957
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-22 01:38:56 +0530

    Add files via upload

---
+++ Migratory Birds.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<Integer> arr) 
+    {
+        int ar[]=new int[6];
+        for(int a:arr)
+        {
+            ar[a]++;
+        }
+        int max=Integer.MIN_VALUE;
+        int ans=-1;
+        for(int i=5;i>=1;i--)
+        {
+            if(ar[i]>=max)
+            {
+                max=ar[i];
+                ans=i;
+
+            }
+
+        }
+        return ans;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr.add(arrItem);
+        }
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
