867a341c9380098c7a5a00b52e0b957323bac99d
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-26 23:17:21 +0530

    Add files via upload

---
+++ Linked Lists Detect a Cycle.java
@@ -0,0 +1,30 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) 
+{
+    if(head==null)
+    {
+        return false;
+    }
+    Node fast=head.next;
+    Node slow=head;
+    while(fast!=null&&fast.next!=null)
+    {
+        if(slow==fast)
+        {
+            return true;
+        }
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return false;
+
+}
